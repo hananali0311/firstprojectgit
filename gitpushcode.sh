a29d14a57e82687bbc12f5039089ab1ba53060e5
hananali0311 <hananali.0311@gmail.com>
2024-03-05 15:08:50 +0400

    gitpushcommand

---
+++ gitpushcode.sh
@@ -0,0 +1,29 @@
+git config --global user.name "hananali0311"
+  522  git config --global user.email "hananali.0311@gmail.com"
+  523  git config --list
+  524  pwd
+  525  mkdir gitdir
+  526  cd gitdir
+  527  git clone https://github.com/hananali0311/firstprojectgit.git
+  528  ls
+  529  cd firstproject
+  530  cd firstprojectgit
+  531  ls
+  532  touch hello.py
+  533  ls
+  534  gedit hello.py
+  535  clear
+  536  ls
+  537  git status
+  538  git add hello.py
+  539  git status
+  540  git commit -m "1st code"
+  541  git status
+  542  git push origin main 
+  543  clear
+  544  git remote set-url origin https://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@github.com/hananali0311/firstprojectgit.git
+  545  git push origin main
+  546  ls
+  547  cat hello.py
+  548  history
+
